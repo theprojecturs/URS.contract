7f86cca7820e9d574d12a983dfe479316f47c0ca
mempooool <mempooool@gmail.com>
2021-09-02 20:03:39 +0900

    Prepare real deploy

--- scripts/deploy-pass.ts
+++ scripts/deploy-pass.ts
@@ -1,29 +1,21 @@
-const { ethers } = require('hardhat');
+import { ethers } from 'hardhat';
+import deployData from '../data/deploy-data.json';
 
-const configs = {
-  name: 'TEST PASS',
-  symbol: 'PASS',
-  baseURI: 'ipfs://QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE',
-};
-
-const main = async () => {
+const main: () => Promise<void> = async () => {
   const [deployer] = await ethers.getSigners();
-
   console.log('Deploying contracts with the account:', deployer.address);
 
   const Pass = await ethers.getContractFactory('contracts/Pass.sol:Pass');
   const contract = await Pass.deploy(
-    configs.name,
-    configs.symbol,
-    configs.baseURI
+    deployData.passContractConfigs.name,
+    deployData.passContractConfigs.symbol,
+    deployData.passContractConfigs.baseURI
   );
 
   await contract.deployed();
   console.log('Contract deployed at:', contract.address);
 };
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
